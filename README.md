9e7e7b8dc2cfc0cf109e2c64f759b22bada41455
Christopher S. Case <chris.case@g33xnexus.com>
2016-11-23 19:46:18 -0600

    big update and code cleanup.

--- README.md
+++ README.md
@@ -1,145 +1,24 @@
 # VueMirror
 
-VueMirror is a [CodeMirror][codemirror] component for VueJS 2.X. It was forked from [surmon-china/vue-codemirror][vue-codemirror] to add browserify support, and clean up the code a bit.
+VueMirror is a [CodeMirror][codemirror] component for VueJS 2.X. It was forked from [surmon-china/vue-codemirror][vue-codemirror]
+to add browserify support, a Grunt build system, and clean up the code a bit.
 
 [codemirror]: https://codemirror.net/
 [vue-codemirror]: https://github.com/surmon-china/vue-codemirror
 
------
+## Installation
 
-Old ReadMe
+Install through npm:
 
------
-
-# Example
-
-[Demo Page](https://surmon-china.github.io/vue-codemirror)
-
-
-# Use Setup
-
-
-### Install vue-codemirror
-
-``` bash
-npm install vue-codemirror --save
-```
-
-### Vue use
-
-``` javascript
-// import with ES6
-import Vue from 'vue'
-// ...
-import CodeMirror from 'vue-codemirror'
-
-
-// require with Webpack/Node.js
-var Vue = require('vue')
-// ...
-var CodeMirror = require('vue-codemirror')
-
-
-// use
-Vue.use(CodeMirror)
-
-
-// --------------------------------------
-
-
-// or use with component(ES6)
-import Vue from 'vue'
-// ...
-import { codemirror } from 'vue-codemirror'
-
-// use
-export default {
-  components: {
-    codemirror
-  }
-}
+```bash
+$ npm install --save-dev vuemirror codemirror
 ```
 
 
-### Use in components
-
-``` html
-<codemirror></codemirror>
 
 
-<!-- component data bind and config in Vue.js1.X -->
-<codemirror :code="code" :options="editorOption"></codemirror>
-<!-- Bidirectional data binding in Vue.js1.X -->
-<codemirror :code.sync="code" :options="editorOption"></codemirror>
+## CodeMirror Documentation
 
-
-<!-- component data bind and config in Vue.js2.X -->
-<codemirror :code="code" :options="editorOption"></codemirror>
-<!-- Bidirectional data binding in Vue.js2.X -->
-<codemirror v-model="code" :options="editorOption"></codemirror>
-<!-- or -->
-<!-- If you need to manually control the data synchronization, you can monitor the code change event like this -->
-<codemirror :code="code" :options="editorOption" @changed="codeChange"></codemirror>
-```
-
-
-``` javascript
-// editor option example:
-export default {
-  data () {
-    return {
-      code: 'const a = 10',
-      editorOption: {
-        tabSize: 4,
-        mode: 'text/javascript',
-        theme: 'base16-dark',
-        lineNumbers: true,
-        line: true,
-        ...
-      }
-    }
-  },
-  // If you need to manually control the data synchronization, parent component needs to explicitly emit an event instead of relying on implicit binding
-  methods: {
-    codeChange(newCode) {
-      console.log(newCode)
-      this.code = newCode
-    }
-  }
-}
-
-// editorOption mode types:
-
-// string mode
-mode: 'text/javascript'
-
-// object mode
-mode: {
-  name: 'javascript',
-  json: true
-}
-```
-
-``` html
-<!-- component config example 2 (Vue.js1.X) -->
-<codemirror :code.sync="css" :options="{ tabSize: 2, mode: 'text/css' }"></codemirror>
-```
-
-``` javascript
-data () {
-  return {
-    css: '.class { display: block }'
-  }
-}
-```
-
-
-# More Config
-
-[Code example](https://github.com/surmon-china/vue-codemirror/tree/master/example)
-
-[Codemirror config APIs](http://codemirror.net/doc/manual.html#config)
-
-[Codemirror themes](http://codemirror.net/demo/theme.html)
-
-[Codemirror language modes](http://codemirror.net/mode/) (MIME types defined)
+* [Codemirror config APIs](http://codemirror.net/doc/manual.html#config)
+* [Codemirror themes](http://codemirror.net/demo/theme.html)
+* [Codemirror language modes](http://codemirror.net/mode/) (MIME types defined)
---
+++ src/codemirror.vue
@@ -0,0 +1,206 @@
+<!--------------------------------------------------------------------------------------------------------------------->
+<!-- CodeMirror Component                                                                                            -->
+<!--------------------------------------------------------------------------------------------------------------------->
+
+<template>
+	<textarea></textarea>
+</template>
+
+<!--------------------------------------------------------------------------------------------------------------------->
+
+<script>
+	//------------------------------------------------------------------------------------------------------------------
+
+	var CodeMirror = require('codemirror/lib/codemirror.js');
+	var CodeMirrorMetas = require('./metas.js');
+
+	//------------------------------------------------------------------------------------------------------------------
+
+	module.exports = {
+		data()
+		{
+			return { content: '' };
+		},
+		props: {
+			code: String,
+			value: String,
+			markerLine: {
+				type: Array,
+				default: function(){ return []; }
+			},
+			unseenLines: Array,
+			marker: {
+				type: Function,
+				default: function()
+				{
+					var marker = document.createElement('div');
+					marker.style.color = '#822';
+					marker.innerHTML = '●';
+					return marker;
+				}
+			},
+			markerUnseenLines: {
+				type: Function,
+				default: function()
+				{
+					var marker = document.createElement('div');
+					marker.style.color = '#FFF';
+					marker.innerHTML = '●';
+					return marker;
+				}
+			},
+			options: {
+				type: Object,
+				default: function()
+				{
+					return {
+						styleActiveLine: true,
+						lineNumbers: true,
+						mode: 'text/javascript',
+						lineWrapping: true
+					};
+				}
+			},
+		},
+		created()
+		{
+			var error;
+			this.options = this.options || {};
+
+			// Theme handling
+			var theme = this.options.theme;
+			theme = theme == 'solarized light' ? 'solarized' : theme;
+
+			// Language handling
+			var language = this.options.mode || 'text/javascript';
+			if(typeof language == 'string')
+			{
+				try
+				{
+					language = CodeMirrorMetas.findModeByMIME(language).mode
+				}
+				catch(exp)
+				{
+					error = new Error(`Configuration error: The CodeMirror language mode '${ language }' is configured incorrectly, or it is not supported. Did you forget to require it before VueMirror?`);
+					error.innerError = exp;
+
+					throw error;
+				} // end try/catch
+			}
+			else
+			{
+				try
+				{
+					language = CodeMirrorMetas.findModeByName(language.name).mode
+				}
+				catch(exp)
+				{
+					error = new Error(`Configuration error: The CodeMirror language mode '${ language }' is configured incorrectly, or it is not supported. Did you forget to require it before VueMirror?`);
+					error.innerError = exp;
+
+					throw error;
+				}
+			} // end if
+		},
+		ready()
+		{
+			this.editor = CodeMirror.fromTextArea(this.$el, this.options);
+			this.editor.setValue(this.code || this.value || this.content);
+
+			this.editor.on('change', (cm) =>
+			{
+				this.content = cm.getValue();
+				this.code = cm.getValue();
+			});
+		},
+		mounted()
+		{
+			this.editor = CodeMirror.fromTextArea(this.$el, this.options);
+			this.editor.setValue(this.code || this.value || this.content);
+
+			this.editor.on('change', (cm, changeObj) =>
+			{
+				this.content = cm.getValue();
+				if(this.$emit)
+				{
+					this.$emit('changed', this.content);
+					this.$emit('input', this.content);
+				} // end if
+			});
+
+			this.editor.on("gutterClick", (cm, n) =>
+			{
+				this.gutterMarkersByClicked(n);
+			});
+
+			if(this.unseenLines)
+			{
+				this.gutterMarkers();
+			} // end if
+		},
+		watch: {
+			code(newVal, oldVal)
+			{
+				const editor_value = this.editor.getValue();
+
+				if(newVal !== editor_value)
+				{
+					var scrollInfo = this.editor.getScrollInfo();
+					this.editor.setValue(newVal);
+					this.content = newVal;
+					this.editor.scrollTo(scrollInfo.left, scrollInfo.top);
+				} // end if
+
+				if(this.unseenLines)
+				{
+					this.gutterMarkers()
+				} // end if
+			},
+			value(newVal, oldVal)
+			{
+				this.unseenLines = [];
+				const editor_value = this.editor.getValue();
+
+				if (newVal !== editor_value)
+				{
+					var scrollInfo = this.editor.getScrollInfo();
+					this.editor.setValue(newVal);
+					this.content = newVal;
+					this.editor.scrollTo(scrollInfo.left, scrollInfo.top);
+				} // end if
+
+				if(this.unseenLines)
+				{
+					this.gutterMarkers()
+				} // end if
+			}
+		},
+		methods: {
+			gutterMarkers()
+			{
+				this.unseenLines.forEach((line) =>
+				{
+					var info = this.editor.lineInfo(line);
+					this.editor.setGutterMarker(line, "breakpoints", info.gutterMarkers ? null : this.markerUnseenLines());
+				});
+			},
+			gutterMarkersByClicked(line)
+			{
+				var info = this.editor.lineInfo(line);
+				this.editor.setGutterMarker(line, "breakpoints",  info.gutterMarkers ? null : this.marker());
+			}
+		}
+	}
+</script>
+
+<!--------------------------------------------------------------------------------------------------------------------->
+
+<style>
+	.CodeMirror,
+	.CodeMirror pre {
+		font-family: Menlo, Monaco, Consolas, "Courier New", monospace!important;
+		padding: 0 20px !important; /* Horizontal padding of content */
+	}
+</style>
+
+<!--------------------------------------------------------------------------------------------------------------------->
